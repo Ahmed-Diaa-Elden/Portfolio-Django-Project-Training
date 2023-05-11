f77e14b712e355dbb15baa5e5a01c4b11ae3a1f1
Ahmed-Diaa-Elden <engdiaa98@gmail.com>
2023-05-11 19:00:58 +0300

    solving issue

--- resume/views.py
+++ resume/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def resume(request):
-    return render(request, "resume.html ")
+    # today = datetime.datetime.now()
+    return render(request, "resume.html")
