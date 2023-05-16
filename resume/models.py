762b4349a5998e54fbe54a5c2405a8218d73e20c
Ahmed-Diaa-Elden <engdiaa98@gmail.com>
2023-05-16 14:48:11 +0300

    CRUD

--- resume/models.py
+++ resume/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+  fName = models.CharField(max_length=255)
+  lName = models.CharField(max_length=255)
+
+
+class Course(models.Model):
+  cName = models.CharField(max_length=255)
+  isFinished = models.BooleanField()
--- resume/views.py
+++ resume/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
+import datetime
 
 # Create your views here.
 def resume(request):
-    # today = datetime.datetime.now()
-    return render(request, "resume.html")
+    today = datetime.datetime.now()
+    return render(request, "resume.html", {"today": today})
