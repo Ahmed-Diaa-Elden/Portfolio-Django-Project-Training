fa4d73b986cf372367876dfe28245525430d9785
Ahmed-Diaa-Elden <engdiaa98@gmail.com>
2023-05-17 05:39:44 +0300

    adding assets

--- resume/models.py
+++ resume/models.py
@@ -5,7 +5,6 @@ class Student(models.Model):
   fName = models.CharField(max_length=255)
   lName = models.CharField(max_length=255)
 
-
 class Course(models.Model):
   cName = models.CharField(max_length=255)
   isFinished = models.BooleanField()
